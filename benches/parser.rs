8cf059d6cabd416ddfc032c1d8c4324695cc5137
Rob Ede <robjtede@icloud.com>
2026-04-04 17:17:01 +0300

    test: add bench

---
+++ benches/parser.rs
@@ -0,0 +1,21 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+
+#[path = "../src/segment.rs"]
+mod segment;
+
+#[path = "../src/decode/mod.rs"]
+mod decode;
+
+fn parse_mummyforced(c: &mut Criterion) {
+    let bytes = std::fs::read("data/mummyforced.sup").expect("read data/mummyforced.sup");
+
+    c.bench_function("parse_frames/mummyforced", |b| {
+        b.iter(|| {
+            let frames = decode::parse_frames(&bytes).expect("parse mummyforced.sup");
+            criterion::black_box(frames);
+        });
+    });
+}
+
+criterion_group!(benches, parse_mummyforced);
+criterion_main!(benches);
